49901dd4f02cef74533646858c571ddd7c572f03
denmcca <denmcca@gmail.com>
2025-12-04 11:39:25 -0700

    Implement debug argument

--- tufs.py
+++ tufs.py
@@ -1,3 +1,12 @@
-from theoretical_use_file_scrubber.app import main
+# from theoretical_use_file_scrubber.app import main
+from app import main
+import argparse
 
-main.main()
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true')
+    return parser.parse_args()
+
+
+main.main(**vars(get_args()))
