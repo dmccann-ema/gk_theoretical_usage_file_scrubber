01d7ca93ebb9bacc5eda53a2ce1a060828dc5716
denmcca <denmcca@gmail.com>
2025-12-04 11:35:18 -0700

    Add app data in package init

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "1.0.1"
+
+APPNAME = "GK Theoretical Use of Raw Materials by Sales File Scrubber"
+DESCRIPTION = "A specialized scrubbing tool for GK Theoretical Use of Raw Materials by Sales files"
