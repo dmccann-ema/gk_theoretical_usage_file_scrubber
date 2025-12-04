1e56964dd29d9a34f54ce36d3e942f7ba865ed6c
denmcca <denmcca@gmail.com>
2025-12-04 11:40:18 -0700

    Change to versio 1.0.2

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 APPNAME = "GK Theoretical Use of Raw Materials by Sales File Scrubber"
 DESCRIPTION = "A specialized scrubbing tool for GK Theoretical Use of Raw Materials by Sales files"
